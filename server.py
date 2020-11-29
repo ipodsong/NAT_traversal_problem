75af828d99f797e7456cff1702a1da0a9d91a202
nonu1017 <55435652+nonu1017@users.noreply.github.com>
2020-11-30 01:15:23 +0900

    Update server.py
    keep alive in 30s 구현함

--- server.py
+++ server.py
@@ -1,17 +1,44 @@
 import sys
 from time import sleep
 ### library ###
-import thread
+import time
+import threading
+import ctrl_socket
+import utils
 
 
 serverPort = 10080
-
+def timecheck():
+    global table_lock
+    while True:
+        termkey = {}
+        table_lock.acquire()
+        for key in IPTABLE:
+            key[1] = key[1] + 1
+            if key[1] > 30:
+                termkey[key] = 1
+        for key in termkey:
+            IPTABLE.del(key)
+            print(key, ' is disconnected')
+        table_lock.release()
+        sleep(1)
+        if termserver:
+            return
+        
 def server():
     """
     Write your code!!!
     """
-    print('server')
-    pass
+    global table_lock, termserver
+    print('server started...')
+    IPTABLE = {}
+    table_lock = threading.lock(); termserver = 0
+    timecheck()
+    server_socket = ctrl_socket.ctrl_socket(('', serverPort))
+    
+    while True:
+        
+    
 
 
 """
