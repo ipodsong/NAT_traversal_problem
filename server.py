249c170d5cdee525e3197b8363b4e70400a294c0
nonu1017 <nonu11411@gmail.com>
2020-11-29 23:34:56 +0900

    init commit

---
+++ server.py
@@ -0,0 +1,23 @@
+import sys
+from time import sleep
+### library ###
+import thread
+
+
+serverPort = 10080
+
+def server():
+    """
+    Write your code!!!
+    """
+    print('server')
+    pass
+
+
+"""
+Don't touch the code below
+"""
+if  __name__ == '__main__':
+    server()
+
+
