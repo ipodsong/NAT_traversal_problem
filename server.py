7991c1636eccd4b4103c0341c4ba980e95f7e8db
nonu1017 <55435652+nonu1017@users.noreply.github.com>
2020-11-30 12:57:32 +0900

    Update server.py

--- server.py
+++ server.py
@@ -88,16 +88,16 @@ def recv_data(s_socket):
                }
     
     while True:
-        data, addr = s_socket.recv_data()  ## 아마도 이 부분 수정해야할지도
+        data = s_socket.recv_data()  ## 아마도 이 부분 수정해야할지도
         
         # if server is terminated
         if termserver:
             return        
         
         # if data is none
-        if data != 0:
-            pass
-        
+        if data == 0:
+            continue
+        data, addr = data
         # unpack data
         mode, unpacked = utils.unpackdata(data.decode())
         
