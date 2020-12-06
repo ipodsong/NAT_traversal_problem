d18f2315957f0763a81421de7b9ff1e1fb95c7f5
nonu1017 <55435652+nonu1017@users.noreply.github.com>
2020-12-07 01:13:18 +0900

    rm list

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ def pack_CID(data):
     ## return : 'CID:{CID}\r\n'
     return 'CID:{}\r\n'.format(data)
 
-# pack request list from client to server
-def pack_req_list(data):
+# pack remove CID to client
+def pack_rm_CID(data):
     ## data : client_ID
     ## return : 'CID:{CID}\r\n'
     return 'CID:{}\r\n'.format(data)
@@ -31,34 +31,23 @@ def pack_keep_alive(data):
     ## return : 'CID:{CID}\r\n'
     return 'CID:{}\r\n'.format(data)
 
-# pack send list from server to client
-def pack_res_list(data):
-    ## data : [[CID1, ADDR1], [CID2, ADDR2], [CID3, ADDR3], ... ]
-    ## return : 'CID:{CID1}\r\nADDR:{ADDR1}\r\nCID:{CID2}\r\nADDR:{ADDR2}\r\nCID:{CID3}\r\nADDR:{ADDR3}\r\n...'
-    table = ''
-    for key in data:
-        ## add 'CID:{CIDi}\r\nADDR:{ADDRi}\r\n
-        table = table + 'CID:{CID}\r\nADDR:{ADDR}\r\n'.format(CID=key[0], ADDR=key[1])
-        
-    return table
+
 
 # dic for send mode
 sendmode = { 0 : pack_CID, \
-             1 : pack_req_list, \
+             1 : pack_rm_CID, \
              2 : pack_chat, \
              3 : pack_exit, \
-             4 : pack_keep_alive, \
-             5 : pack_res_list \
+             4 : pack_keep_alive \
             }
 # pack data
 def make_data(mode, data):
     # mode
-    # 0 : send CID
-    # 1 : send req list
+    # 0 : send create CID
+    # 1 : send rm CID
     # 2 : send chat
     # 3 : send keep alive
     # 4 : send exit
-    # 5 : send res list
     
     # send data format
     data = 'mode:{mode}\r\n\r\n ' \
@@ -84,8 +73,8 @@ def unpack_CID(data):
     ## return CID
     return rmcol(data)
 
-# unpack request list from client
-def unpack_req_list(data):
+# unpack remove CID from server
+def unpack_rm_CID(data):
     ## data : 'CID:{CID}'
     ## return CID
     return rmcol(data)
@@ -109,34 +98,22 @@ def unpack_keep_alive(data):
     ## return CID
     return rmcol(data)
 
-# unpack sent response list from server to client
-def unpack_res_list(data):
-    ## data : 'CID:{CID1}\r\nADDR:{ADDR1}\r\nCID:{CID2}\r\nADDR:{ADDR2}\r\n...CID:{CIDn}\r\nADDR:{ADDRn}'
-    ## return : [[CID1, ADDR1], [CID2, ADDR2], ..., [CIDn, ADDRn]]
-    unpack = rmrn(data)
-    unpack_list = []
-    for i in range(0, len(unpack), 2):
-        ##append [CIDk, ADDRk] to unpack_list, k = i/2
-        unpack_list.append([rmcol(unpack[i]), rmcol(unpack[i+1])])
-        
-    return unpack_list
+
 
 # dic for recv mode
 recvmode = { '0' : unpack_CID, \
-             '1' : unpack_req_list, \
+             '1' : unpack_rm_CID, \
              '2' : unpack_chat, \
              '3' : unpack_exit, \
-             '4' : unpack_keep_alive, \
-             '5' : unpack_res_list \
+             '4' : unpack_keep_alive \
             }      
 def unpack_data(data):
     # mode
     # 0 : recv CID
-    # 1 : recv req list
+    # 1 : recv rm CID
     # 2 : recv chat
     # 3 : recv keep alive
     # 4 : recv exit
-    # 5 : recv res list
     # data : 'mode:{}\r\n\r\n{DATA}\r\n\r\n'
     
     # split data by \r\n\r\n
